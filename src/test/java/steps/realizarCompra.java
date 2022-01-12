d43d28a1796a5ec6ac72c3bd133b4884ff3739fd
vandres777 <42923341+vandres777@users.noreply.github.com>
2022-01-12 08:29:27 -0500

    Se actualiza espera de carga de pagina

--- src/test/java/steps/realizarCompra.java
+++ src/test/java/steps/realizarCompra.java
@@ -6,6 +6,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 
 import io.cucumber.java.en.*;
@@ -13,63 +15,63 @@ import io.cucumber.java.en.*;
 public class realizarCompra {
 	WebDriver driver;
 
-	@Given("^abro navegador en modo incognito$")
+	@Given("^Abro navegador en modo incognito$")
 	public void abro_navegador_en_modo_incognito() {
 		System.setProperty("webdriver.chrome.driver", "src/test/resources/driver/chromedriver.exe");
 		ChromeOptions options = new ChromeOptions();
 		options.addArguments("--incognito");
 		driver = new ChromeDriver(options);
-		//System.out.println("Navegador se abre en modo incognito");
+		// System.out.println("Navegador se abre en modo incognito");
 
 	}
 
-	@When("^me encuentro en la pagina de mercado libre y acepto cookies$")
+	@When("^Me encuentro en la pagina de mercado libre y acepto cookies$")
 	public void me_encuentro_en_la_pagina_de_mercado_libre() {
 		driver.manage().window().maximize();
 		driver.navigate().to("https://www.mercadolibre.com.co/");
 		String ActualTitle = driver.getTitle();
 		String ExpectedTitle = "Mercado Libre Colombia - Envíos Gratis en el día";
 		Assert.assertEquals(ExpectedTitle, ActualTitle);
-		//System.out.println("se abre la página");
+		// System.out.println("se abre la página");
 		String title = driver.getTitle();
 		System.out.println("El título de la página es:" + title);
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		driver.findElement(By.xpath("(//button[normalize-space()='Entendido'])[1]")).click();
 	}
 
-	@Then("^busco el producto monitor$")
+	@Then("^Busco el producto monitor$")
 	public void busco_el_producto_monitor() {
 		driver.findElement(By.name("as_word")).sendKeys("monitor pc 27 pulgadas");
 		driver.findElement(By.xpath("//button/div")).click();
-		//driver.findElement(By.partialLinkText("Monitor gamer curvo ViewSonic VX2768-PC-mhd led 27")).click();
 		driver.findElement(By.partialLinkText("Samsung Cf390 Series 27 ''monitor De Escritorio Curvado")).click();
 	}
 
-	@Then("^una vez seleccionado el producto voy a comprarlo como cliente nuevo$")
+	@Then("^Una vez seleccionado el producto voy a comprarlo como cliente nuevo$")
 	public void una_vez_seleccionado_el_producto_voy_a_comprarlo_como_cliente_nuevo() {
 		driver.findElement(By.xpath("//span[normalize-space()='Comprar ahora']")).click();
-		
-		//driver.findElement(By.xpath("//button[@class='andes-button andes-button--loud']//span[@class='andes-button__content'][normalize-space()='Comprar ahora']")).click();
 		driver.findElement(By.xpath("//a[normalize-space()='Soy nuevo']")).click();
 	}
-	
-	@Then("^lleno el formulario e ingreso (.*) (.*) (.*) (.*)$")
-		public void lleno_el_formulario_e_ingreso_andres_y_leon(String firstName, String lastName, String email,String password) throws InterruptedException {
-		
-Thread.sleep(1000);
-		/*WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-		wait.until(ExpectedConditions.elementToBeClickable(By.id("password")));*/
+
+	@Then("^Lleno el formulario e ingreso (.*) (.*) (.*) (.*)$")
+	public void lleno_el_formulario_e_ingreso_andres_y_leon(String firstName, String lastName, String email,
+			String password) throws InterruptedException {
+
+		// Thread.sleep(1000);
+		WebDriverWait wait = new WebDriverWait(driver, 10);
+		wait.until(ExpectedConditions.or(ExpectedConditions.visibilityOfElementLocated(By.id("firstName"))));
 
 		driver.findElement(By.id("firstName")).sendKeys(firstName);
 		driver.findElement(By.id("lastName")).sendKeys(lastName);
 		driver.findElement(By.id("email")).sendKeys(email);
 		driver.findElement(By.id("password")).sendKeys(password);
 	}
-	@Then("acepto terminos y condiciones")
+
+	@Then("Acepto terminos y condiciones")
 	public void acepto_terminos_y_condiciones() {
 		driver.findElement(By.xpath("//label[contains(text(),'Acepto los')]")).click();
 	}
-	@Then("^hago clic en continuar$")
+
+	@Then("^Hago clic en continuar$")
 	public void hago_clic_en_continuar() {
 		driver.findElement(By.xpath("//span[@class='andes-button__content']")).click();
 
--- target/test-classes/features/MercadoLibre.feature
+++ target/test-classes/features/MercadoLibre.feature
@@ -1,13 +1,13 @@
 Feature: Buscar producto en mercado libre
 
   Scenario Outline: Busqueda de texto para validar el titulo de la pagina resupesta
-    Given abro navegador en modo incognito
-    When me encuentro en la pagina de mercado libre y acepto cookies
-    Then busco el producto monitor
-    Then una vez seleccionado el producto voy a comprarlo como cliente nuevo
-    And lleno el formulario e ingreso <firstName> <lastName> <email> <password>
-    Then acepto terminos y condiciones
-    And hago clic en continuar
+    Given Abro navegador en modo incognito
+    When Me encuentro en la pagina de mercado libre y acepto cookies
+    Then Busco el producto monitor
+    Then Una vez seleccionado el producto voy a comprarlo como cliente nuevo
+    And Lleno el formulario e ingreso <firstName> <lastName> <email> <password>
+    Then Acepto terminos y condiciones
+    And Hago clic en continuar
 
     Examples: 
       | firstName | lastName | email              | password  |
