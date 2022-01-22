9f9245abded70d1a1f9a8cb3c20ece96101d4540
vandres777 <42923341+vandres777@users.noreply.github.com>
2022-01-22 12:04:54 -0500

    Se actualiza feature

--- src/test/java/steps/realizarCompra.java
+++ src/test/java/steps/realizarCompra.java
@@ -4,6 +4,7 @@ import java.time.Duration;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -16,7 +17,7 @@ public class realizarCompra {
 	WebDriver driver;
 
 	@Given("^Abro navegador en modo incognito$")
-	public void abro_navegador_en_modo_incognito() {
+	public void Abro_navegador_en_modo_incognito() {
 		System.setProperty("webdriver.chrome.driver", "src/test/resources/driver/chromedriver.exe");
 		ChromeOptions options = new ChromeOptions();
 		options.addArguments("--incognito");
@@ -26,7 +27,7 @@ public class realizarCompra {
 	}
 
 	@When("^Me encuentro en la pagina de mercado libre y acepto cookies$")
-	public void me_encuentro_en_la_pagina_de_mercado_libre() {
+	public void Me_encuentro_en_la_pagina_de_mercado_libre() {
 		driver.manage().window().maximize();
 		driver.navigate().to("https://www.mercadolibre.com.co/");
 		String ActualTitle = driver.getTitle();
@@ -39,23 +40,23 @@ public class realizarCompra {
 	}
 
 	@Then("^Busco el producto monitor$")
-	public void busco_el_producto_monitor() {
+	public void Busco_el_producto_monitor() {
 		driver.findElement(By.name("as_word")).sendKeys("monitor pc 27 pulgadas");
 		driver.findElement(By.xpath("//button/div")).click();
 		driver.findElement(By.partialLinkText("Samsung Cf390 Series 27 ''monitor De Escritorio Curvado")).click();
 	}
 
 	@Then("^Una vez seleccionado el producto voy a comprarlo como cliente nuevo$")
-	public void una_vez_seleccionado_el_producto_voy_a_comprarlo_como_cliente_nuevo() {
+	public void Una_vez_seleccionado_el_producto_voy_a_comprarlo_como_cliente_nuevo() {
 		driver.findElement(By.xpath("//span[normalize-space()='Comprar ahora']")).click();
 		driver.findElement(By.xpath("//a[normalize-space()='Soy nuevo']")).click();
 	}
 
-	@And("^Lleno el formulario e ingreso (.*) (.*) (.*) (.*)$")
-	public void lleno_el_formulario_e_ingreso_andres_y_leon(String firstName, String lastName, String email,
+	@And("^Diligencio el formulario de registro con (.*) (.*) (.*) (.*)$")
+	public void Diligencio_el_formulario_de_registro_con_andres_y_leon(String firstName, String lastName, String email,
 			String password) throws InterruptedException {
 
-		// Thread.sleep(1000);
+		//Thread.sleep(1000);
 
 		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 		wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("firstName")));
@@ -67,23 +68,28 @@ public class realizarCompra {
 	}
 
 	@Then("^Acepto terminos y condiciones$")
-	public void acepto_terminos_y_condiciones() throws InterruptedException {
+	public void Acepto_terminos_y_condiciones() throws InterruptedException {
 		driver.findElement(By.xpath("//label[contains(text(),'Acepto los')]")).click();
 	}
 
 	@And("^Valido si aparece captcha$")
-	public void valido_si_aparece_captcha() {
+	public void Valido_si_aparece_captcha() {
 
-		if (driver.findElements(By.xpath("//label[@id='recaptcha-anchor-label']")).size() != 0) {
-			System.out.println("Element present");
+
+		if (driver.findElements(By.xpath("//*[@id=\"recaptcha-anchor-label\"]/text()")).size() != 0) {
+			System.out.println("recaptcha visible, se debe hacer clic de forma manual");
 		} else
-			System.out.println("Element not present");
+			System.out.println("recaptcha no visible, flujo continua");
 
 	}
 
 	@Then("^Hago clic en continuar$")
-	public void hago_clic_en_continuar() {
+	public void Hago_clic_en_continuar() {
 		driver.findElement(By.xpath("//span[@class='andes-button__content']")).click();
+
+	}
+	@Then("^Se visualiza pagina para envio de codigo$")
+	public void Se_visualiza_pagina_para_envio_de_codigo() {
 
 	}
 
--- src/test/resources/features/MercadoLibre.feature
+++ src/test/resources/features/MercadoLibre.feature
@@ -1,14 +1,15 @@
 Feature: Buscar producto en mercado libre
 
-  Scenario Outline: Busqueda de texto para validar el titulo de la pagina resupesta
+  Scenario Outline: Comprar monitor en mercado libre
     Given Abro navegador en modo incognito
     When Me encuentro en la pagina de mercado libre y acepto cookies
     Then Busco el producto monitor
     Then Una vez seleccionado el producto voy a comprarlo como cliente nuevo
-    And Lleno el formulario e ingreso <firstName> <lastName> <email> <password>
+    And Diligencio el formulario de registro con <firstName> <lastName> <email> <password>
     Then Acepto terminos y condiciones
     And Valido si aparece captcha
     And Hago clic en continuar
+    Then Se visualiza pagina para envio de codigo
 
     Examples: 
       | firstName | lastName | email              | password  |
